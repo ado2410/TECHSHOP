ef8182e436a24d1c6aa519e020547b7b76f9d88e
jennydo2000 <adork2410@gmail.com>
2020-12-03 12:14:46 +0700

    fix some issues

--- src/gui/nhanvien/detail/SoldListController.java
+++ src/gui/nhanvien/detail/SoldListController.java
@@ -3,6 +3,7 @@ package gui.nhanvien.detail;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
+import util.GUILoader;
 import util.Util;
 
 public class SoldListController {
@@ -29,5 +30,13 @@ public class SoldListController {
 	
 	public void setPrice(String price) {
 		this.price.setText(price);
+	}
+	
+	@FXML
+	private void onAction() {
+		GUILoader loader = GUILoader.load("gui/hoadon/detail/GUI");
+		gui.hoadon.detail.GUIController guiController = (gui.hoadon.detail.GUIController) loader.getController();
+		guiController.initInfo(id.getText());
+		GUILoader.loadToScene(loader);
 	}
 }
